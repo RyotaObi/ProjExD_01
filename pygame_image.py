48c1e9382a01137abc974c3a058d164d9c2e8737
obiryota <c0a22041f5@edu.teu.ac.jp>
2023-11-21 15:45:03 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #背景画像surfaceの生成
-    kk_img = pg.image.load("ex01/fig/3png") #こうかとんの画像surfaceの生成
+    kk_img = pg.image.load("ex01/fig/3.png") #こうかとんの画像surfaceの生成
     kk_img = pg.transform.flip(kk_img,True,False) #こうかとんの反転
+    kk_imgs = [kk_img, pg.transform.rotozoom(kk_img,10,1.0)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0]) #背景画像の表示
-        screen.blit(kk_img, [300, 200]) 
+        screen.blit(kk_imgs[tmr%2], [300, 200]) 
         pg.display.update()
         tmr += 1        
         clock.tick(10)
