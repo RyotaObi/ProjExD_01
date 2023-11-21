5cee6a2bcf3368a7f5992c10cd06547e7d5d84df
obiryota <c0a22041f5@edu.teu.ac.jp>
2023-11-21 15:59:06 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -15,11 +15,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #背景画像の表示
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0]) #背景画像の表示
+        screen.blit(bg_img, [1600-x, 0])
         screen.blit(kk_imgs[tmr%2], [300, 200]) 
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
